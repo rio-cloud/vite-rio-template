30eea90ea46b991c0ef8118aeb2a2680e0dcc852
Robin Kipsch <robin.kipsch@rio.cloud>
2022-12-19 17:07:50 +0100

    (fix) formatting

--- src/routes/DefaultRedirect.tsx
+++ src/routes/DefaultRedirect.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-
 import { DEFAULT_ROUTE, routes } from './Router';
 
 const isUnknownRoute = (routeNames: string[], pathname: string): boolean =>
